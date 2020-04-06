8da45802dbbfe89810f8395f36173b13bbdfc129
padamshrestha <padamshres@gmail.com>
2020-04-06 09:11:41 -0700

    Add Sales route
    Signed-off-by: padamshrestha <padamshres@gmail.com>

--- apps/stackline/src/app/app-routing.module.ts
+++ apps/stackline/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OverviewComponent } from './overview/overview.component';
+import { SalesComponent } from './sales/sales.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'overview' },
-  { path: 'overview', component: OverviewComponent }
+  { path: 'overview', component: OverviewComponent },
+  { path: 'sales', component: SalesComponent }
 ];
 
 @NgModule({
